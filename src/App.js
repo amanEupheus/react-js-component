01a3113448f5392484647965442234f773f08658
amanEupheus <amankumar@eupheus.in>
2022-12-10 14:00:03 +0530

    react js component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Blog } from "./components/blog";
+import { Content } from "./components/content";
+import { Faq } from "./components/faq";
+import { Feature } from "./components/feacture";
+import { Footer } from "./components/footer";
+import { Header } from "./components/header";
+import { Nav } from "./components/nav";
+import { Pricing } from "./components/pricing";
+import { Statistic } from "./components/statics";
+import { Step } from "./components/step";
+import { Team } from "./components/team";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Nav/>
+      <Routes>
+        <Route exact path="/" element={<Header />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/features" element={<Feature />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/statistic" element={<Statistic />} />
+        <Route path="/content" element={<Content />} />
+        <Route path="/step" element={<Step />} />
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,116 @@
+import { Blog } from "./blog";
+import { Content } from "./content";
+import { Faq } from "./faq";
+import { Feature } from "./feacture"
+import { Pricing } from "./pricing";
+import { Statistic } from "./statics";
+import { Step } from "./step";
+import { Team } from "./team";
+
+export const Header = () => {
+    return (
+
+        <>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="flex flex-col items-center justify-between lg:flex-row">
+          <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
+            <div className="max-w-xl mb-6">
+              <div>
+                <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-green-400">
+                  Brand new
+                </p>
+              </div>
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                The quick, brown fox
+                <br className="hidden md:block" />
+                jumps over{' '}
+                <span className="inline-block text-green-400">
+                  a lazy dog
+                </span>
+              </h2>
+              <p className="text-base text-gray-700 md:text-lg">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae. explicabo.
+              </p>
+            </div>
+            <div className="flex flex-col items-center md:flex-row">
+              <a
+                href="/"
+                className="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-green-400 hover:bg-green-700 focus:shadow-outline focus:outline-none"
+              >
+                <span className="mr-3">Book apponment</span>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-4"
+                >
+                  <polyline
+                    fill="none"
+                    stroke="currentColor"
+                    strokeMiterlimit="10"
+                    points="4,4 22,4 19,14 4,14 "
+                  />
+                  <circle
+                    cx="4"
+                    cy="22"
+                    r="2"
+                    strokeLinejoin="miter"
+                    strokeLinecap="square"
+                    stroke="none"
+                    fill="currentColor"
+                  />
+                  <circle
+                    cx="20"
+                    cy="22"
+                    r="2"
+                    strokeLinejoin="miter"
+                    strokeLinecap="square"
+                    stroke="none"
+                    fill="currentColor"
+                  />
+                  <polyline
+                    fill="none"
+                    stroke="currentColor"
+                    strokeMiterlimit="10"
+                    points="1,1 4,4 4,14 2,18 23,18 "
+                  />
+                </svg>
+              </a>
+              <a
+                href="/"
+                aria-label=""
+                className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-green-700"
+              >
+                Get 15% discount
+              </a>
+            </div>
+          </div>
+          <div className="relative lg:w-1/2">
+            <img
+              className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+              src="https://img.freepik.com/premium-photo/thumbs-up-smile-happy-doctor-woman-physician-with-yes-thumbsup-support-portrait-goal-health-approval-hands-success-hospital-worker-welcome-motivation-winner-thank-you_590464-86299.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+      <Feature/>
+      <Content/>
+      <Pricing/>
+      <Faq/>
+      <Step/>
+      <Team/>
+      <Blog/>
+      <Statistic/>
+      
+       
+      </div>
+      </>
+    );
+  };
